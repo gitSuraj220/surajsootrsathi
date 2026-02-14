f08ebd9917f4c1b21ee79ad43d66468267bcf5c8
gitSuraj220 <93196336+gitSuraj220@users.noreply.github.com>
2026-02-14 16:25:30 +0530

    Update HeroSection.tsx
    सुनिश्चित मानदेय

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -118,7 +118,7 @@ const HeroSection = () => {
           </div>
           <div className="flex items-center gap-2">
             <div className="w-2 h-2 rounded-full bg-accent" />
-            <span>मानदेय सुनिश्चित</span>
+            <span>सुनिश्चित मानदेय</span>
           </div>
         </motion.div>
       </div>
